7f22d0e92a18e4ae814313151cc3718161174e2c
xsanfz <sergey.myamin@gmail.com>
2025-03-17 22:00:31 +0400

    Оформил новый код

--- main.py
+++ main.py
@@ -1,12 +1,52 @@
 from dotenv import load_dotenv
 import os
-from echobot import EchoBot
-
+import ptbot
+from pytimeparse import parse
 load_dotenv()
+
+
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+TG_CHAT_ID = os.getenv('TG_CHAT_ID')
+bot = ptbot.Bot(TELEGRAM_TOKEN)
+
+
+def render_progressbar(total, iteration, prefix='', suffix='', length=20, fill='█', zfill='░'):
+    iteration = min(total, iteration)
+    percent = "{0:.1f}"
+    percent = percent.format(100 * (iteration / float(total)))
+    filled_length = int(length * iteration // total)
+    pbar = fill * filled_length + zfill * (length - filled_length)
+    return '{0} |{1}| {2}% {3}'.format(prefix, pbar, percent, suffix)
+
+
+def wait(chat_id, question):
+    delay = parse(question)
+    if delay is None:
+        bot.send_message(chat_id, "Неверный формат времени")
+        return
+
+    message_id = bot.send_message(chat_id, render_progressbar(delay, delay, prefix="Осталось:"))
+
+    bot.create_countdown(delay, notify_progress, chat_id=chat_id, message_id=message_id, delay=delay)
+
+    bot.create_timer(delay, choose, chat_id=chat_id, question=question)
+
+
+def notify_progress(secs_left, chat_id, message_id,delay):
+    progressbar = render_progressbar(delay, delay - secs_left, prefix="Осталось:")
+    bot.update_message(chat_id, message_id, progressbar)
+
+
+def choose(chat_id, question):
+    message = "Время вышло"
+    bot.send_message(chat_id, message)
 
 
 if __name__ == "__main__":
-    bot = EchoBot(TELEGRAM_TOKEN)
-    bot.reply_on_message()
+
+    bot = ptbot.Bot(TELEGRAM_TOKEN)
+    bot.reply_on_message(wait)
     bot.run_bot()
+
+
+
